3ef950ed11f1a83d2c8c7041d5b74c97a7003d75
smiroshnikov <sergeim.qa@gmail.com>
2016-07-21 15:17:15 +0300

    house

--- Geometry/Geometry/Form1.Designer.cs
+++ Geometry/Geometry/Form1.Designer.cs
@@ -29,24 +29,40 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DrawingBoard));
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.Color.White;
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(942, 449);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
             // 
             // DrawingBoard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(942, 449);
+            this.Controls.Add(this.Canvas);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "DrawingBoard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Геометрические Фигуры";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
     }
 }
 
--- Geometry/Geometry/Form1.cs
+++ Geometry/Geometry/Form1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace Geometry
@@ -15,6 +8,27 @@ namespace Geometry
         public DrawingBoard()
         {
             InitializeComponent();
+            DrawOnMyCanvas();
         }
+        public void DrawOnMyCanvas()
+        {
+            Bitmap bmp = new Bitmap(Canvas.Width, Canvas.Height); //  initialize object required to work with pixels 
+            Graphics graph = Graphics.FromImage(bmp); // initialize a DRAWING SURFACE!
+            Pen pen = new Pen(Color.Purple); // initializing pen is also required 
+
+            graph.DrawRectangle(pen , 100, 100, 200, 200);
+            
+            graph.DrawLine(pen, 100, 100, 200, 10);
+            graph.DrawLine(pen, 200, 10, 300, 100);
+            Canvas.Image = bmp; // PictureBox method 
+
+            
+
+
+        }
+
+
+
+
     }
 }
